90a534a86ee277b283e982245c262b252ad44dbf
hodaziat <hoda.ziat@fau.de>
2026-09-12 23:35:08 +0200

    add name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import Header from './Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <section id="center">
+        <Header />
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
---
+++ frontend/src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+   <>
+         <div><h1> welcome to group B 26 react platform</h1></div>
+        <div> <h1 style={{color:"red"}} > Teacher Name : Ahmad </h1></div>
+        <div> <h1 style={{color:"blue"}}> Student Name : Zaid </h1></div>
+    </>
+  )
+}
+
+export default Header
